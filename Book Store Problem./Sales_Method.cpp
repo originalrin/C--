b614692138c393193d3b909a164d4e9344a8f1c3
Rin <rinspoofer@gmail.com>
2025-04-08 07:53:02 -0400

    Updated to account for if user inputs nothing.

--- Book Store Problem./Sales_Method.cpp
+++ Book Store Problem./Sales_Method.cpp
@@ -6,24 +6,27 @@ each ISBN.
 */
 int main() {
 
-Sales_item item1, total, wow;
+    Sales_item item1, total, wow;
 
-if (std::cin >> item1) {
-    int cnt = 1;
-    while(std::cin >> total) {
-        if(item1.isbn()==total.isbn()) {
-            item1+=wow; 
-            cnt++;
+    if (std::cin >> item1) {
+        int cnt = 1;
+        while(std::cin >> total) {
+            if(item1.isbn()==total.isbn()) {
+                item1+=wow; 
+                cnt++;
+            }
+            else {
+                std::cout << "You made " << cnt << " purchase(s) of " << item1.isbn() << std::endl; 
+                item1 = total;
+                cnt = 1;
+            }
+            
         }
-        else {
-            std::cout << "You made " << cnt << " purchases of " << item1.isbn() << std::endl; 
-            item1 = total;
-            cnt = 1;
-        }
-        
+        std::cout << "You made " << cnt << " purchases of " << item1.isbn() << std::endl;
+    } 
+    else {
+        std::cerr << "No data?!" << std::endl; 
+        return -1;
     }
-    std::cout << "You made " << cnt << " purchases of " << item1.isbn() << std::endl;
-} 
-
     return 0;
 }
