eb4ff69bf99e62f1c102ee9c6bd37fcf64430be2
Rin <rinspoofer@gmail.com>
2025-04-17 11:10:32 -0400

    ufo.hpp

---
+++ UFO codecademy/ufo.hpp
@@ -0,0 +1,9 @@
+#include <vector>
+#include <string>
+// Declare functions
+
+void display_misses(int misses);
+
+void greet();
+void display_status(std::vector<char> incorrect, std::string answer);
+void end_game(std::string answer, std::string codeword);
